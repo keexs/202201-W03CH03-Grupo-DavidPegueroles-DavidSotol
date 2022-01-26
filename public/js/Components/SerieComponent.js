4e4dcd8d6cf6b3e44b01bdb350fd57e07edab0fb
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-01-26 22:07:59 +0100

    Imported ImageComponent and added a method to generate one

--- public/js/Components/SerieComponent.js
+++ public/js/Components/SerieComponent.js
@@ -1,13 +1,16 @@
 import Component from "./Component.js";
+// eslint-disable-next-line import/no-unresolved
+import ImageComponent from "./ImageComponent.js";
 
 class SerieComponent extends Component {
   serie;
 
-  constructor(parentElement, className, htmlTag, serie) {
+  constructor(parentElement, className, serie, htmlTag = "li") {
     super(parentElement, className, htmlTag);
 
     this.serie = serie;
 
+    this.addImage();
     this.generateHTML();
   }
 
@@ -16,6 +19,12 @@ class SerieComponent extends Component {
       <h4 class="serie__title">${this.serie.name}</h4>
       <p class="serie__info">${this.serie.creator} (${this.serie.year})</p>
     `;
+  }
+
+  addImage() {
+    const parentElement = this.element;
+    // eslint-disable-next-line no-unused-vars
+    const insertImage = new ImageComponent(parentElement, this.serie);
   }
 }
 
