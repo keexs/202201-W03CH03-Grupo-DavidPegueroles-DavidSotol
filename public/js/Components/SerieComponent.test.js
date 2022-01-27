16b82c6dd9473ead00016cee3efb57672b16e46e
David Soto <davidsotosendarrubias@gmail.com>
2022-01-27 19:46:05 +0100

    SerieComponent second test pass

--- public/js/Components/SerieComponent.test.js
+++ public/js/Components/SerieComponent.test.js
@@ -26,4 +26,29 @@ describe("Given SerieComponent", () => {
       expect(container.querySelector("li")).not.toBeNull();
     });
   });
+
+  describe("When it recives a serie with 'The Sopanos' name", () => {
+    test("Then shoud render 'The Sopranos' in serie__title", () => {
+      const serie = {
+        id: 1,
+        name: "The Sopranos",
+        creator: "David Chase",
+        year: 1999,
+        poster: "ImageSubstitute",
+        watched: true,
+        score: 5,
+        emmies: 21,
+      };
+      const container = document.createElement("div");
+      const title = "The Sopranos";
+      const serieComponent = new SerieComponent(
+        container,
+        "class",
+        serie,
+        "li"
+      );
+
+      expect(serie.name).toBe(title);
+    });
+  });
 });
