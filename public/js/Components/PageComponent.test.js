6b1dfbf207eba132e486e47bf206e903bcf4e2fe
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-01-27 21:05:08 +0100

    Added test to test PageComponent element creation function

--- public/js/Components/PageComponent.test.js
+++ public/js/Components/PageComponent.test.js
@@ -1,1 +1,13 @@
 import PageComponent from "./PageComponent.js";
+
+describe("Given a PageComponent component", () => {
+  describe("When it receives 'div'", () => {
+    test("Then it should create a div element to the dom", () => {
+      const container = document.createElement("div");
+
+      new PageComponent(container);
+
+      expect(container.querySelector(".container")).not.toBeNull();
+    });
+  });
+});
